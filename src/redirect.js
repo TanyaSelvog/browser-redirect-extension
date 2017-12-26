8b14b5d9f2b69b29dc650fbba6a04e23ef4c0e80
Tanya Selvog <tanyaselvog@gmail.com>
2017-12-25 21:23:28 -0600

    updated redirect to include all facebook

--- src/redirect.js
+++ src/redirect.js
@@ -1,7 +1,10 @@
-browser.webRequest.onBeforeRequest.addListener(function(details){
-	if (details.url === "https://www.facebook.com/") {
-		return {redirectUrl: browser.extension.getURL("index.html")};
-	}
+
+browser.webRequest.onBeforeRequest.addListener(
+	function(details){
+		const url = new URL (details.url);
+		if (url.hostname.endsWith("facebook.com")){
+			return {redirectUrl: browser.extension.getURL("index.html")};
+		}
 	}, {
 		urls: ["<all_urls>"],
 		types: ["main_frame"]
